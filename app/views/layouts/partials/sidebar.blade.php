7051de564f8c807a1a7bed5985c3c52c43147bf3
Purevdorj <ncst.puje@gmail.com>
2014-10-06 23:38:19 +0800

    changed contact form

--- app/views/layouts/partials/sidebar.blade.php
+++ app/views/layouts/partials/sidebar.blade.php
@@ -4,10 +4,9 @@
             <h6 style="font-style: italic; color: #bbb">Мэдээлэл</h6>
             <h4><span class="glyphicon glyphicon-th-list"></span> Ангилал</h4>
             <div class="list-group">
-                <a href="#" class="list-group-item"> <span class="badge">5</span>Улс төр</a>
-                <a href="#" class="list-group-item"> <span class="badge">2</span>Нийгэм</a>
-                <a href="#" class="list-group-item"> <span class="badge">5</span>Даатгал</a>
-                <a href="#" class="list-group-item"> <span class="badge">5</span>Оюутолгой</a>
+                @foreach(Tag::get() as $tag)
+                    <a href="#" class="list-group-item">{{ $tag->tag }}</a>
+                @endforeach()
             </div>
         </article>
     </aside>
